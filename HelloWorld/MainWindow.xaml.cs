4ad33eea31fdb3cb21d54b7f076d4dec0cd0d7b0
Ori <OriBennett@gmail.com>
2023-12-03 10:45:20 +0200

    Did TextBlock Review in front and code behind. Doing Labels next

--- HelloWorld/MainWindow.xaml.cs
+++ HelloWorld/MainWindow.xaml.cs
@@ -23,11 +23,22 @@ namespace HelloWorld
             myButton.Content = "Welcome to the program. Click me!";
             helloTB.Foreground = Brushes.BlueViolet;
             helloTB.FontWeight = FontWeights.Bold;
+            //TextBlock textBlock = new TextBlock();
+            //textBlock.FontSize = 50;
+            //textBlock.Text = "Hello";
+            //textBlock.Inlines.Add(" Added new stuff behind with Inline");
+            //textBlock.Inlines.Add(new Run(" Run text that I added in code behind")
+            //{
+            //    Foreground = Brushes.BlueViolet,
+            //    TextDecorations = TextDecorations.Underline
+            //});
+            //textBlock.TextWrapping = TextWrapping.Wrap;
+            //this.Content = textBlock;
         }
 
         private void Hyperlink_RequestNavigate(object sender, RequestNavigateEventArgs e)
         {
-           // System.Diagnostics.Process.Start(e.Uri.AbsoluteUri);  Doesn't work like in the tutorial and I'm not sure why
+           //System.Diagnostics.Process.Start(e.Uri.AbsoluteUri);  Doesn't work like in the tutorial and I'm not sure why probably because I don't have a default browser. Also might be that I'm not in framework.
         }
     }
 }
