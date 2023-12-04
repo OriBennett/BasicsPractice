a97a8df95e67021c20a0a1ca7c9d551cce009da1
Ori <OriBennett@gmail.com>
2023-12-04 13:49:21 +0200

    CatYears in XAML code with some code behind

---
+++ CatYears/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CatYears
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CatAgeInYears_KeyUp(object sender, KeyEventArgs e)
+        {
+            const double CATAGEMULTIPLIER = 5.4;
+            if ((sender as TextBox) != null)
+            {
+                double catAge;
+                if (IsNumeric((sender as TextBox).Text))
+                {
+                    double.TryParse((sender as TextBox).Text, out catAge);
+                    CatAgeTB.Text = "Your cat is " + catAge * CATAGEMULTIPLIER + " years old.";
+                }
+                else
+                {
+                    CatAgeTB.Text = "Please enter a number";
+                }
+            }
+        }
+
+        private bool IsNumeric(string text)
+        {
+            return double.TryParse(text, out _);
+        }
+    }
+}
