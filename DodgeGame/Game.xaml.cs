86291b372a15b73c24dc7b0e9ef44b5537a98fb6
Ori <OriBennett@gmail.com>
2023-12-09 16:10:14 +0200

    Mocked up a basic board load function

--- DodgeGame/Game.xaml.cs
+++ DodgeGame/Game.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -34,5 +35,28 @@ namespace DodgeGame
         {
             gameText.Text = text;
         }
+
+        public void InitializeBoard()
+        {
+            Image goodie = new Image(/*goodieImagae*/) {/*Random location in Canvas*/ };
+            Image[] baddies = new Image[10];
+            this.AddVisualChild(goodie);
+
+            for (int i = 0; i < baddies.Length; i++)
+            {
+                baddies[i] = new Image(/*BaddiesImage*/) {/*Random location in Canvas Not on Goodie*/ };
+                this.AddVisualChild(baddies[i]);
+            }
+        }
+        //public void InitializeBoardFromSave(/*Some sort of data object with locations for goddie and baddies*/)
+        //{
+        //    Image goodie = new Image(/*goodieImagae*/) {/*Random location in Canvas*/ };
+        //    Image[] baddies = new Image[10];
+
+        //    for (int i = 0; i < baddies.Length; i++)
+        //    {
+        //        baddies[i] = new Image(/*BaddiesImage*/) {/*Random location in Canvas Not on Goodie*/ };
+        //    }
+        //}
     }
 }
