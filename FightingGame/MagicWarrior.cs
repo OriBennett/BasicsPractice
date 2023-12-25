9965407c7e4e165a5f30a31014977e5596ba0b4f
Ori <OriBennett@gmail.com>
2023-12-25 18:05:10 +0200

    Added a new MagicWarrior class with a Teleport interface

---
+++ FightingGame/MagicWarrior.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FightingGame
+{
+    internal class MagicWarrior : Warrior
+    {
+        int teleportChance = 0;
+        public Teleports Teleports = new CanTeleport(); //so can be changed when teleportation is blocked
+
+        public MagicWarrior(int teleportChance = 0, string name = "Warrior", double health=0, double attkMax = 0, double blockMax = 0) 
+            : base(name, health, attkMax, blockMax)
+        {
+            this.teleportChance = teleportChance;
+        }
+        public override double Block()
+        {
+            Random rnd = new Random();
+            int rndDodge = rnd.Next(1,100);
+            if (rndDodge < this.teleportChance && Teleports.teleport() != "Fails at Teleporting")
+            {
+                Console.WriteLine($"{this.Name} {this.Teleports.teleport()}");
+                return 10000; //higher than expected attack
+            }
+            return base.Block();
+        }
+    }
+}
--- FightingGame/Program.cs
+++ FightingGame/Program.cs
@@ -5,8 +5,8 @@
         static void Main(string[] args)
         {
             Warrior thor = new Warrior("Thor", 100, 26, 10);
-            Warrior hulk = new Warrior("Hulk", 100, 26, 10);
-            Battle.StartFight(thor, hulk);
+            MagicWarrior loki = new MagicWarrior(50, "Loki", 75, 20, 10);
+            Battle.StartFight(thor, loki);
         }
     }
 }
