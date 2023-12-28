7a992bb3cdb179c5aa55166a11130a8446f0d3ed
Ori <OriBennett@gmail.com>
2023-12-28 15:22:39 +0200

    Thread basics and Sleep

---
+++ ThreadPractice/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace ThreadPractice
+{
+    internal class Program
+    {
+        static void Print1()
+        {
+            for (int i = 0; i < 1000; i++)
+            {
+                Console.Write(1);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Thread thread = new Thread(Print1 );
+            thread.Start();
+            for (int i = 0; i < 1000; i++)
+            {
+                Console.Write(0);
+            }
+            int num = 1;
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write(num);
+                Thread.Sleep(1000);
+                num++;
+            }
+
+            
+        }
+    }
+}
