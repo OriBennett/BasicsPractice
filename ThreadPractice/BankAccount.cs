c48b8b05808b0f10bc717d68b58df38c1371c084
Ori <OriBennett@gmail.com>
2024-01-02 11:48:17 +0200

    Practiced lock

---
+++ ThreadPractice/BankAccount.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThreadPractice
+{
+    class BankAccount
+    {
+        private Object acctLock = new Object();
+        double Balance { get; set; }
+        string Name { get; set; }
+
+        public BankAccount(double balance=0, string name= "No Name")
+        {           
+            Balance = balance;
+            Name = name;
+        }
+
+        public double Withdraw(double amount)
+        {
+            if (Balance < amount)
+            {
+                Console.WriteLine($"Sorry, ${Balance} in account");
+                return Balance;
+            }
+            else
+            {
+                lock (acctLock)
+                {
+                    Balance -= amount;
+                    Console.WriteLine($"Removed ${amount}, new balance is ${Balance}");
+                    return Balance;
+                }
+            }
+        }
+    }
+}
