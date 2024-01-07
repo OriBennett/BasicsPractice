775a2729bbf80c674879307909cf278000793d00
Ori <OriBennett@gmail.com>
2024-01-07 16:09:52 +0200

    set up the databases

--- DatabasePractice/MainWindow.xaml.cs
+++ DatabasePractice/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +24,7 @@ namespace DatabasePractice
         public MainWindow()
         {
             InitializeComponent();
+            string connectionString = ConfigurationManager.ConnectionStrings["DatabasePractice.Properties.Settings.PracticeDatabaseConnectionString"].ConnectionString;
         }
     }
 }
