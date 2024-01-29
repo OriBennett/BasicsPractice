37273589a67a4d0fe6c033a7ca7eb04b88dbc39f
Ori <OriBennett@gmail.com>
2024-01-29 12:31:21 +0200

    OO core principals practice init and start

---
+++ OOCore1/Program.cs
@@ -0,0 +1,35 @@
+﻿
+ITalkable ta = factory();
+ta.Talk();
+
+ITalkable factory()
+{
+    return new Dog();
+}
+
+public interface ITalkable
+{
+    void Talk();
+}
+
+public class Cat : ITalkable
+{
+    public void Talk()
+    {
+        Console.WriteLine("Meow");
+    }
+}
+public class Dog : ITalkable
+{
+    public void Talk()
+    {
+        Console.WriteLine("Woof");
+    }
+}
+public class Human : ITalkable
+{
+    public void Talk()
+    {
+        Console.WriteLine("Hello");
+    }
+}
